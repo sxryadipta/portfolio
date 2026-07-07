315ab91cc96f79640aa9c9f90ff5422363745757
Suryadipta Ghosh <gsuryadipta@gmail.com>
2026-07-07 11:16:17 +0530

    Fix Techstack

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,5 +1,27 @@
+import react from '../assets/svg/react.svg'
+import javascript from '../assets/svg/javaScript.svg'
+import html5 from '../assets/svg/HTML5.svg'
+import css3 from '../assets/svg/CSS3.svg'
+import tailwind from '../assets/svg/tailwindcss.svg'
+import nodejs from '../assets/svg/nodejs.svg'
+import git from '../assets/svg/git.svg'
+import bash from '../assets/svg/bash.svg'
+import vercel from '../assets/svg/vercel.svg'
+import nextjs from '../assets/svg/next.svg'
+import mysql from '../assets/svg/mysql.svg'
+import astro from '../assets/svg/astro.svg'
+
 export const techStack = [
-  { name: 'React', icon: null },
-  { name: 'JavaScript', icon: null },
-  { name: 'Tailwind CSS', icon: null },
+  { name: 'React', icon: react },
+  { name: 'JavaScript', icon: javascript },
+  { name: 'HTML5', icon: html5 },
+  { name: 'CSS3', icon: css3 },
+  { name: 'Tailwind CSS', icon: tailwind },
+  { name: 'Node.js', icon: nodejs },
+  { name: 'Git', icon: git },
+  { name: 'Bash', icon: bash },
+  { name: 'Vercel', icon: vercel },
+  { name: 'Nextjs', icon: nextjs },
+  { name: 'MySQL', icon: mysql },
+  { name: 'Astro', icon: astro }
 ]
