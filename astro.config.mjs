caeccc5f0f6198efd452207e722717a8b78d6f5e
Suryadipta Ghosh <gsuryadipta@gmail.com>
2026-09-13 18:47:17 +0530

    Add index.html

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://sxryadipta.vercel.app', 
-  output: 'static',
-  build: {
-    format: 'directory',
-  },
-});
